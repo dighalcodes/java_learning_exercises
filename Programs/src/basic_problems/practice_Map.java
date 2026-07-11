706224ed0374af0dd3d94ca73117a78d00d37095
Dighal Saha <dighal.saha.dev@gmail.com>
2026-07-11 21:05:01 +0530

    Problem-2 of HashMap added

--- Programs/src/basic_problems/practice_Map.java
+++ Programs/src/basic_problems/practice_Map.java
@@ -7,30 +7,61 @@ import java.util.Map;
 
 public class practice_Map {
 
-    public static int mostFrequent(List<Integer> input) {
+    // Method of problem-1
+    // public static int mostFrequent(List<Integer> input) {
 
-        Map<Integer, Integer> freq = new HashMap<>();
+    // Map<Integer, Integer> freq = new HashMap<>();
+    // for (int i = 0; i < input.size(); i++) {
+    // freq.put(input.get(i), freq.getOrDefault(input.get(i), 0) + 1);
+    // }
+
+    // int maxCount = 0;
+    // int whichKey = 0;
+    // for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+    // if (entry.getValue() > maxCount) {
+    // maxCount = entry.getValue();
+    // whichKey = entry.getKey();
+    // }
+    // }
+    // return whichKey;
+
+    // }
+
+    // Method of problem-2
+    public static int[] twoSum(List<Integer> input, int target) {
+        Map<Integer, Integer> seen = new HashMap<>();
+
         for (int i = 0; i < input.size(); i++) {
-            freq.put(input.get(i), freq.getOrDefault(input.get(i), 0) + 1);
-        }
+            int current = input.get(i);
+            int complement = target - current;
 
-        int maxCount = 0;
-        int whichKey = 0;
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
-            if (entry.getValue() > maxCount) {
-                maxCount = entry.getValue();
-                whichKey = entry.getKey();
+            if(seen.containsKey(complement)){
+                return new int[] {seen.get(complement), i};
             }
+            seen.put(current, i);
         }
-        return whichKey;
-
+        return new int[] {};
     }
 
     public static void main(String[] args) {
-        List<Integer> input = new ArrayList<>(List.of(3, 1, 4, 1, 5, 3, 3));
 
-        System.out.println("Most Frequent: " + mostFrequent(input));
-        
+        // Problem 1 — Easy: "Find the Most Frequent Element"
+        // Problem Statement:
+        // You are given a List<Integer>. Write a method that returns the element that
+        // appears the most number of times. If there is a tie, return any one of them.
+        // List<Integer> input = new ArrayList<>(List.of(3, 1, 4, 1, 5, 3, 3));
+        // System.out.println("Most Frequent: " + mostFrequent(input));
+
+        // Problem 2 — Easy-Medium: "Two Sum"
+        // Problem Statement:
+        // You are given a List<Integer> and a target integer. Write a method that
+        // returns the indices of the two numbers that add up to the target. You may
+        // assume exactly one solution exists.
+        List<Integer> input = new ArrayList<>(List.of(2,7,11,15));
+        int[] result = twoSum(input, 9);
+        System.out.println("Indices: ["+result[0]+", "+result[1]+"]");
+
+
     }
 
 }
