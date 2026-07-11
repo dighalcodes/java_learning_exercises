366449d8d0f5617782e3aeda5a906b7917f1e66c
Dighal Saha <dighal.saha.dev@gmail.com>
2026-07-11 15:46:50 +0530

    added problem 1 of HashMap

--- Programs/src/basic_problems/practice_Map.java
+++ Programs/src/basic_problems/practice_Map.java
@@ -1,37 +1,36 @@
 package basic_problems;
 
 import java.util.HashMap;
-import java.util.TreeMap;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class practice_Map {
+
+    public static int mostFrequent(List<Integer> input) {
+
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int i = 0; i < input.size(); i++) {
+            freq.put(input.get(i), freq.getOrDefault(input.get(i), 0) + 1);
+        }
+
+        int maxCount = 0;
+        int whichKey = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                whichKey = entry.getKey();
+            }
+        }
+        return whichKey;
+
+    }
+
     public static void main(String[] args) {
-        Map<String, Integer> scores = new HashMap<>();
+        List<Integer> input = new ArrayList<>(List.of(3, 1, 4, 1, 5, 3, 3));
 
-        // Map<String, Integer> TreeMap = new TreeMap<>();
-        // Map<Integer, Integer> freqMap = new HashMap<>();
-
-        // method-1
-
-        scores.put("Alice", 90);
-        scores.put("Bob", 75);
-        scores.put("Alice", 95);
-        System.out.println(scores);
-
-        /*
-         * // method-2
-         * System.out.println(scores.get("Alice"));
-         * 
-         * // method-3
-         * System.out.println(scores.containsKey("Charlie"));
-         * 
-         * // method-4
-         * System.out.println(scores.getOrDefault("Charlie", 1));
-         * 
-         * // method-5
-         * scores.remove("Bob");
-         */
-
+        System.out.println("Most Frequent: " + mostFrequent(input));
+        
     }
 
 }
